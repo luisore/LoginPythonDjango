25d615d235730c660d2cff8e8727e2e5b213e055
luisore <luissk47@gmail.com>
2024-12-03 17:02:40 -0300

    email validation

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from gfg import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from . tokens import generate_token
 
 # Create your views here.
 
@@ -44,6 +49,7 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
+        myuser.is_active = False
 
         myuser.save()
 
@@ -56,6 +62,29 @@ def signup(request):
         from_email = settings.EMAIL_HOST_USER
         to_list = [myuser.email]
         send_mail(subject,message,from_email,to_list,fail_silently=True)
+
+
+        #Email Address Confirmation Email
+
+        current_site = get_current_site(request)
+
+        email_subject= "Confirm your email @ DJ - Django Login!!"
+        message2 = render_to_string('email_confirmation.html',{
+            "name":myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token':generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email]
+        )
+
+        email.fail.silently = True
+        email.send()
+
 
         return redirect ('signin')
 
@@ -84,3 +113,19 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged Out Succesfully!")
     return redirect('home')
+
+
+
+def activate(request, uidb64,token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+        if myuser is not None and generate_token.check_token(myuser,token):
+            myuser.is_active = True
+            myuser.save
+            login(request,myuser)
+            return redirect('home')
+        else:
+            return render(request, 'activation_failed.html')
--- gfg/info.py
+++ gfg/info.py
@@ -7,6 +7,7 @@ EMAIL_HOST_PASSWORD = 'Django123'
 EMAIL_PORT = 587
 """
 # Looking to send emails in production? Check out our Email API/SMTP product!
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
 EMAIL_HOST_USER = '7b4a3ed1de6748'
 EMAIL_HOST_PASSWORD = '285803118682cd'
